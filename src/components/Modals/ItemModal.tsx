4a5684c0c7fca31536f897a0ce4878d949eee8ef
viktor8551 <viktor.walser@live.com>
2022-11-24 05:18:58 +0100

    small changes to ItemModal

--- src/components/Modals/ItemModal.tsx
+++ src/components/Modals/ItemModal.tsx
@@ -17,7 +17,7 @@ const ItemModal: FC<ItemModalProps> = ({setModalOpen, setItems}) => {
     })
 
     return (
-        <div className="flex justify-center items-center absolute left-0 top-0 w-screen h-screen bg-black opacity-80">
+        <div className="flex justify-center items-center absolute left-0 top-0 w-screen h-screen bg-black bg-opacity-80">
             <div className="w-[280px] bg-white p-6">
                 <input
                     className="w-full mb-5 border-2 border-blue-400 p-2"
@@ -33,7 +33,10 @@ const ItemModal: FC<ItemModalProps> = ({setModalOpen, setItems}) => {
                         CANCEL
                     </button>
                     <button
-                        onClick={() => addItem({ name: input })}
+                        onClick={() => {
+                            addItem({ name: input })
+                            setModalOpen(false)
+                        }}
                         className="bg-blue-400 px-4 py-2">
                         ADD
                     </button>
